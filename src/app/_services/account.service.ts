2488b3633a5563eee854f488db447dd3dfa628c5
ceniza <andreidjionne@email.com>
2026-05-17 23:49:39 +0800

    fixing alerts

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -35,11 +35,13 @@ export class AccountService {
   }
 
   logout() {
-    this.http.post<any>(`${baseUrl}/revoke-token`, {}, { withCredentials: true }).subscribe();
-    this.stopRefreshTokenTimer();
+    if (this.accountValue) {
+        this.http.post<any>(`${baseUrl}/revoke-token`, {}, { withCredentials: true }).subscribe();
+        this.stopRefreshTokenTimer();
+    }
     this.accountSubject.next(null);
     this.router.navigate(['/account/login']);
-  }
+}
 
   refreshToken() {
     return this.http.post<any>(`${baseUrl}/refresh-token`, {}, { withCredentials: true })
