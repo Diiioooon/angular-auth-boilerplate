9b2374d3e61ba8bde5c115fb00e1efde358b0d1f
ceniza <andreidjionne@email.com>
2026-05-18 00:13:00 +0800

    try

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-import { AccountService } from '@app/_services';
+import { AccountService, AlertService } from '@app/_services';
 
 @Component({
   templateUrl: 'login.component.html',
@@ -12,13 +12,13 @@ export class LoginComponent implements OnInit {
   form!: FormGroup;
   submitting = false;
   submitted = false;
-  errorMessage = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private accountService: AccountService,
+    private alertService: AlertService,
     private cdr: ChangeDetectorRef
   ) {}
 
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-    this.errorMessage = '';
+    this.alertService.clear();
     if (this.form.invalid) { return; }
     this.submitting = true;
     this.accountService.login(this.f.email.value, this.f.password.value)
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
           this.router.navigateByUrl(returnUrl);
         },
         error: error => {
-          this.errorMessage = error;
+          this.alertService.error(error);
           this.submitting = false;
           this.cdr.detectChanges();
         }
