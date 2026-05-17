7e1008733253874d4a56e7efd3724e5f6abc5aea
ceniza <andreidjionne@email.com>
2026-05-17 23:13:59 +0800

    fix alert not showing on login error

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-import { AccountService, AlertService } from '@app/_services';
+import { AccountService } from '@app/_services';
 
 @Component({
   templateUrl: 'login.component.html',
@@ -12,14 +12,13 @@ export class LoginComponent implements OnInit {
   form!: FormGroup;
   submitting = false;
   submitted = false;
+  errorMessage = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private accountService: AccountService,
-    private alertService: AlertService,
-    private cdr: ChangeDetectorRef
+    private accountService: AccountService
   ) {}
 
   ngOnInit() {
@@ -33,7 +32,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-    this.alertService.clear();
+    this.errorMessage = '';
 
     if (this.form.invalid) { return; }
 
@@ -46,11 +45,8 @@ export class LoginComponent implements OnInit {
           this.router.navigateByUrl(returnUrl);
         },
         error: error => {
-          setTimeout(() => {
-            this.alertService.error(error);
-            this.submitting = false;
-            this.cdr.detectChanges();
-          }, 0);
+          this.errorMessage = error;
+          this.submitting = false;
         }
       });
   }
